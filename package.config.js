f0bf3c8db4ea35bab6db5027ba6cdd3c88a3b4bd
amekusa <hello@amekusa.com>
2021-09-30 01:06:16 +0900

    Add watch tasks, Add docs support

--- package.config.js
+++ package.config.js
@@ -1,15 +1,17 @@
 // ------ Edit the config as you like ------------------
 const conf = {
-  name:           "my-package",
-  version:        "0.1.0",
-  description:    "",
+  name:           'my-package',
+  version:        '0.1.0',
+  description:    '',
   keywords:       [],
-  main:           "bundle.js",
-  es6_main:       "src/Main.js",
-  github_user:    "", // github username if your repo is on github.com
-  bitbucket_user: "", // bitbucket username if your repo is on bitbucket.org
-  author:         "",
-  license:        "",
+  main:           'bundle.js',   // entry point
+  es6_main:       'src/Main.js', // es6 entry point
+  test:           'test.js',     // unit test file
+  docs:           true,          // support docs?
+  github_user:    '', // github username if your repo is on github.com
+  bitbucket_user: '', // bitbucket username if your repo is on bitbucket.org
+  author:         '',
+  license:        '',
   private:        false
 };
 // ====== That's it. Do not touch below this line ======
@@ -36,12 +38,21 @@ const pkg = {
     watch:          `npm-watch`,
     test:           `c8 mocha --enable-source-maps`,
     codecov:        `c8 report --reporter=text-lcov > coverage.lcov && codecov`,
-    docs:           `npm run docs:gen && npm run docs:publish`,
-    'docs:gen':     `npm run docs:clean && jsdoc -c jsdoc.json && cd docs/${conf.name} && ln -sfn $npm_package_version latest`,
-    'docs:clean':   `rm -rf docs/${conf.name}/$npm_package_version`,
-    'docs:publish': `git subtree push --prefix docs/${conf.name} origin gh-pages`,
     clean:          `rm -f '${conf.main}' '${conf.main}.map' && find . -name '.DS_Store' -not -path '*/node_modules/*' | xargs rm -r`,
     build:          `rollup -c`
+  },
+  watch: {
+    test: {
+      inherit: true,
+      patterns: [
+        conf.test,
+        conf.main
+      ],
+    },
+    build: {
+      inherit: true,
+      patterns: 'src'
+    }
   },
   repository: null,
   bugs:       null,
@@ -52,6 +63,24 @@ const pkg = {
   private:  conf.private
 };
 
+// support docs
+if (conf.docs) {
+  pkg.scripts.docs            = `npm run docs:gen && npm run docs:publish`;
+  pkg.scripts['docs:gen']     = `npm run docs:clean && jsdoc -c jsdoc.json && cd docs/${conf.name} && ln -sfn $npm_package_version latest`;
+  pkg.scripts['docs:clean']   = `rm -rf docs/${conf.name}/$npm_package_version`;
+  pkg.scripts['docs:publish'] = `git subtree push --prefix docs/${conf.name} origin gh-pages`;
+
+  pkg.watch['docs:gen'] = {
+    inherit: true,
+    patterns: [
+      'src',
+      'README.md',
+      'jsdoc.json'
+    ]
+  };
+}
+
+// set repository URLs
 let baseUrl;
 if (conf.bitbucket_user)   baseUrl = `bitbucket.org/${conf.bitbucket_user}/${conf.name}`;
 else if (conf.github_user) baseUrl = `github.com/${conf.github_user}/${conf.name}`;
@@ -61,6 +90,7 @@ if (baseUrl) {
   pkg.homepage   = `https://${baseUrl}#readme`;
 }
 
+// defaults
 if (!pkg.license) pkg.license = 'UNLICENSED';
 
 // remove null properties
@@ -87,14 +117,13 @@ fs.open('./package.json', 'w', (err, fd) => {
     throw err;
   }
   fs.write(fd, r, err => {
-    if (err) {
-      console.error(`failed to write package.json`);
-      throw err;
-    }
-    fs.close(fd, err => {
-      if (err) console.error(err);
-      console.log(`package.json generated.`);
+    fs.close(fd, () => {
+      if (err) {
+        console.error(`failed to write package.json`);
+        throw err;
+      }
+      console.log(`package.json has been written`);
       console.log(r);
-    })
+    });
   });
 });
